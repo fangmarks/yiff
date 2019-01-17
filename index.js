8747dfdaafe90e5742dbb2e469b19e6952e5b547
Awoobis Mac <sxphx2002@gmail.com>
2019-01-17 22:34:28 +0100

    e621 and e926 workingm somewhat

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = async = {
+
+    e621: require('./src/e621'),
+    e926: require('./src/e926') ,
+    furrybot: require('./src/furrybot'),
+    sheri: require('./src/sheri'),
+    // center: require('./src/center')
+
+
+}
