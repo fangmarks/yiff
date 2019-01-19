4e7de41a118c9167b65a320c470ac652788ad54e
Awoobis Mac <sxphx2002@gmail.com>
2019-01-19 19:12:32 +0100

    added version check

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 module.exports = async = {
 
+    version: require('./package.json').version,
     e621: require('./src/e621'),
     e926: require('./src/e926') ,
     furrybot: require('./src/furrybot'),
