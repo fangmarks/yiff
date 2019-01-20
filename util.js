b634386172be73a05970913539f47c675a3a2519
Awoobis Mac <sxphx2002@gmail.com>
2019-01-20 18:33:50 +0100

    utils edit

--- util.js
+++ util.js
@@ -2,11 +2,20 @@
     API Wrapper written by codepupper
 */
 const c = require('chalk')
+let log = console.log
 
 module.exports = {
+    /**
+     * 
+     * @param {string} text 
+     */
     log: function(text){
 
         console.log(c.hex('#a11eff')(`[Yiff Module] `) + c.hex('#ff8d28')(text))
 
+    },
+    invalid: function(){
+        throw new Error(c.hex('#a11eff')(`[Yiff Module] `) + c.hex('#ff8d28')('The API Key you provided is invalid.'))
+        
     }
 }
