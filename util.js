47f37d500617db5aa2a3746794f5bc941a2f0ea3
ryden <rythewuff@gmail.com>
2019-05-15 19:42:47 +0200

    apparently I have changes here

--- util.js
+++ util.js
@@ -3,16 +3,22 @@
 */
 const c = require('chalk');
 let log = console.log;
+let p = require('./package.json')
+
+
 
 module.exports = {
 	/**
-     * 
-     * @param {string} text 
-     */
-	log     : function(text) {
+	 * 
+	 * @param {string} text 
+	 */
+	log: function (text) {
 		console.log(c.hex('#a11eff')(`[Yiff Module] `) + c.hex('#ff8d28')(text));
 	},
-	invalid : function() {
+	invalid: function () {
 		throw new Error(c.hex('#a11eff')(`[Yiff Module] `) + c.hex('#ff8d28')('The API Key you provided is invalid.'));
+	},
+	get useragent() {
+		return `yiff/${p.version} (codepupper)`;
 	}
 };
