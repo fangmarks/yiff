07ec6728ae432c10f09ab78a0f9be5f64ce77631
ryden <code@theendlessweb.com>
2019-05-27 16:37:48 +0200

    I am starting to dislike apis for bots

--- index.js
+++ index.js
@@ -2,24 +2,29 @@
     API Wrapper written by codepupper
 */
 const axios = require('axios');
+const dotenv = require('dotenv')
+dotenv.config()
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-let { fox } = require('./src/fox');
+let {
+	fox
+} = require('./src/fox');
 
 module.exports = async = {
-	version  : require('./package.json').version,
-	e621     : require('./src/e621'),
-	e926     : require('./src/e926'),
-	furrybot : require('./src/furrybot'),
+	version: require('./package.json').version,
+	e621: require('./src/e621'),
+	e926: require('./src/e926'),
+	furrybot: require('./src/furrybot'),
 	/**
-     * @returns {Promise<string>} The Link to the Image
-     */
-	fox      : async function() {
+	 * @returns {Promise<string>} The Link to the Image
+	 */
+	fox: async function () {
 		let response = await axios.get('https://randomfox.ca/floof');
 		let data = response.data;
 		return data.image;
 	},
-	sheri    : require('./src/sheri'),
-	shibe    : require('./src/shibe')
+	sheri: require('./src/sheri'),
+	shibe: require('./src/shibe')
 
 	/* Coming Soon: 
     
