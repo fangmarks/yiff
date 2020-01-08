c713d77c12499f6467bf3dcce3989f5c806b78b4
alexazf <dmitriyev.alexey@gmail.com>
2020-01-09 00:17:44 +0600

    refactor(e621, package): added request method and some changes in configs

--- src/e621.js
+++ src/e621.js
@@ -1,66 +1,66 @@
-let axios = require('axios');
-let util = require('../util');
+const axios = require('axios');
+const util = require('../util');
+
+/**
+ * 
+ * @param {String} request - The tags you want to search for
+ * @param {Object} options - 
+ */
+const request = async function (requestTags, {
+	isCubAllowed = false
+}) {
+	let response = await axios.get(`https://e621.net/post/index.json`, {
+		headers: {
+			'user-agent': this.useragent || util.useragent()
+		},
+		params: {
+			tags: requestTags + ' order:random' + (isCubAllowed ? '' : ' -young')
+		}
+	}).catch(error => {
+		console.log(error.response)
+	});
+
+	if (!response.data[0]) throw new Error('There was no Image found with those tags');
+
+	let {
+		tags,
+		source,
+		score,
+		fav_count,
+		file_url,
+		artist,
+		id
+	} = response.data[0];
+
+	return {
+		tags,
+		source,
+		score,
+		fav_count,
+		artist,
+		image: file_url,
+		postID: id
+	}
+}
 
 module.exports = async = {
-	setUserAgent: function(software) {
+	setUserAgent: function (software) {
 		this.useragent = util.useragent(software);
 	},
 	/**
 	 * 
-	 * @param {string} request - The tags you want to search for | max 4
+	 * @param {string} tags - The tags you want to search for | max 4
 	 */
-	noCubFilter: async function(request) {
-		let url = `https://e621.net/post/index.json`;
-		let response = await axios.get(url, {
-			headers: {
-				'user-agent': this.useragent || util.useragent()
-			},
-			params: {
-				tags: `${request} order:random`
-			}
-		});
-		if (!response.data[0]) throw new Error('There was no Image found with those tags');
-		let { tags, source, score, fav_count, file_url, artist, id } = response.data[0];
-		let data = {
-			tags: tags,
-			source: source,
-			score: score,
-			fav_count: fav_count,
-			image: file_url,
-			artist: artist,
-			postID: id
-		};
-
-		return data;
+	noCubFilter: async function (tags) {
+		request(tags, { isCubAllowed: true })
 	},
 
 	/**
 	 * 
-	 * @param {string} request - The tags you want to search for | max 3
+	 * @param {string} tags - The tags you want to search for | max 3
 	 */
-	CubFilter: async function(request) {
-		let url = `https://e621.net/post/index.json`;
-		let response = await axios.get(url, {
-			headers: {
-				'user-agent': this.useragent || util.useragent()
-			},
-			params: {
-				tags: `${request} order:random -young`
-			}
-		});
-		if (!response.data[0]) throw new Error('There was no Image found with those tags');
-		let { tags, source, score, fav_count, file_url, artist, id } = response.data[0];
-		let data = {
-			tags: tags,
-			source: source,
-			score: score,
-			fav_count: fav_count,
-			image: file_url,
-			artist: artist,
-			postID: id
-		};
-
-		return data;
+	CubFilter: async function (tags) {
+		request(tags, { isCubAllowed: false })
 	}
 };
 
