b43bd5ffefbc3dd4083ace659a94592b10765dfe
ry <rythewuff@gmail.com>
2020-03-07 23:11:30 +0100

    fixing e621 and e926 modules after api changes

--- src/e621.js
+++ src/e621.js
@@ -1,67 +1,70 @@
-const axios = require('axios');
-const util = require('../util');
+const axios = require("axios");
+const util = require("../util");
 
 /**
- * 
+ *
  * @param {String} request - The tags you want to search for
- * @param {Object} options - 
+ * @param {Object} options -
  */
-const request = async function (requestTags, {
-	isCubAllowed = false
-}) {
-	let response = await axios.get(`https://e621.net/post/index.json`, {
-		headers: {
-			'user-agent': this.useragent || util.useragent()
-		},
-		params: {
-			tags: requestTags + ' order:random' + (isCubAllowed ? '' : ' -young')
-		}
-	}).catch(error => {
-		console.log(error.response)
-	});
+const request = async function(requestTags, { isCubAllowed = false }) {
+  let response = await axios
+    .get(`https://e621.net/posts.json`, {
+      headers: {
+        "user-agent": this.useragent || util.useragent()
+      },
+      params: {
+        limit: "1",
+        tags: requestTags + " order:random" + (isCubAllowed ? "" : " -young")
+      }
+    })
+    .catch(error => {
+      console.log(error.response);
+    });
 
-	if (!response.data[0]) throw new Error('There was no Image found with those tags');
+  let posts = response.data["posts"][0];
 
-	let {
-		tags,
-		source,
-		score,
-		fav_count,
-		file_url,
-		artist,
-		id
-	} = response.data[0];
+  //console.log(posts);
 
-	return {
-		tags,
-		source,
-		score,
-		fav_count,
-		artist,
-		image: file_url,
-		postID: id
-	}
-}
+  if (!posts) throw new Error("There was no Image found with those tags");
+  let artists;
+  if (posts.tags.artist.length === 0) artists = ["unknown_artist"];
+  else artists = posts.tags.artists;
+
+  return {
+    postID: posts.id,
+    page: `https://e621.net/posts/${posts.id}`,
+    tags: "Deprecated, see e621 Page for details.",
+    sources: posts.sources,
+    score: posts.score,
+    fav_count: posts.fav_count,
+    artist: posts.tags.artist,
+    image: posts.file.url,
+    md5: posts.file.md5,
+    UA: util.useragent()
+
+    // image: file_url
+  };
+};
 
 module.exports = async = {
-	setUserAgent: function (software) {
-		this.useragent = util.useragent(software);
-	},
-	/**
-	 * 
-	 * @param {string} tags - The tags you want to search for | max 4
-	 */
-	noCubFilter: async function (tags) {
-		return await request(tags, { isCubAllowed: true })
-	},
+  setUserAgent: function(software) {
+    this.useragent = util.useragent(software);
+  },
+  /**
+   *
+   * @param {string} tags - The tags you want to search for | max 4
+   */
+  noCubFilter: async function(tags) {
+    return await request(tags, { isCubAllowed: true });
+  },
 
-	/**
-	 * 
-	 * @param {string} tags - The tags you want to search for | max 3
-	 */
-	CubFilter: async function (tags) {
-		return await request(tags, { isCubAllowed: false })
-	}
+  /**
+   *
+   * @param {string} tags - The tags you want to search for | max 3
+   */
+  CubFilter: async function(tags) {
+    return await request(tags, { isCubAllowed: false });
+  }
 };
 
 /*
--- src/e926.js
+++ src/e926.js
@@ -1,49 +1,62 @@
-const axios = require('axios');
-const util = require('../util');
+const axios = require("axios");
+const util = require("../util");
+
+/**
+ *
+ * @param {String} request - The tags you want to search for
+ * @param {Object} options -
+ */
+const requests = async function(requestTags, { isCubAllowed = false }) {
+  let response = await axios
+    .get(`https://e926.net/posts.json`, {
+      headers: {
+        "user-agent": this.useragent || util.useragent()
+      },
+      params: {
+        limit: "1",
+        tags: requestTags + " order:random" + (isCubAllowed ? "" : " -young")
+      }
+    })
+    .catch(error => {
+      console.log(error.response);
+    });
+
+  let posts = response.data["posts"][0];
+
+  //console.log(posts);
+
+  if (!posts) throw new Error("There was no Image found with those tags");
+  let artists;
+  if (posts.tags.artist.length === 0) artists = ["unknown_artist"];
+  else artists = posts.tags.artists;
+
+  return {
+    postID: posts.id,
+    page: `https://e926.net/posts/${posts.id}`,
+    tags: posts.tags,
+    sources: posts.sources,
+    score: posts.score,
+    fav_count: posts.fav_count,
+    artist: posts.tags.artist,
+    image: posts.file.url,
+    md5: posts.file.md5,
+    UA: util.useragent()
+
+    // image: file_url
+  };
+};
 
 module.exports = async = {
-	setUserAgent: function (software) {
-		this.useragent = util.useragent(software);
-	},
-
-	/**
-	 * 
-	 * @param {String} requestTags - The tags you want to search for
-	 */
-	request: async function (requestTags) {
-		let response = await axios.get(`https://e926.net/post/index.json`, {
-			headers: {
-				'user-agent': this.useragent || util.useragent()
-			},
-			params: {
-				tags: `${requestTags} order:random`
-			}
-		}).catch(error => {
-			console.log(error.response)
-		});
-
-		if (!response.data[0]) throw new Error('There was no Image found with those tags');
-
-		let {
-			tags,
-			source,
-			score,
-			fav_count,
-			file_url,
-			artist,
-			id
-		} = response.data[0];
-
-		return {
-			tags,
-			source,
-			score,
-			fav_count,
-			artist,
-			image: file_url,
-			postID: id
-		}
-	}
+  setUserAgent: function(software) {
+    this.useragent = util.useragent(software);
+  },
+  /**
+   *
+   * @param {string} tags - The tags you want to search for | max 4
+   */
+  request: async function(tags) {
+    return await requests(tags, { isCubAllowed: true });
+  }
 };
 
 /*
