3ce1fde2909f92e8d209563c230f0bfe41dc0b2f
ryden <howl+git@werewolf.computer>
2020-04-16 05:14:13 +0200

    add gitignore stuff

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ module.exports = {
   furrybot: require("./src/furrybot"),
   e621: require("./src/e621"),
   e926: require("./src/e926"),
+  shibe: require("./src/shibe"),
+  sheri: require("./src/sheri"),
 };
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 let yiff = require("./index");
-let x = new yiff.e926();
+let x = new yiff.sheri({ apikey: "3a007f2dc7f31c36185cb208d0d5c4a93b465ab4" });
 
 async function y() {
-  return await x.get("werewolf male/male");
+  return await x.sixtynine;
 }
 y().then((r) => {
   console.log(r);
