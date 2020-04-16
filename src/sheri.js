a795d45a17fff387accb516fdf3801f76af9d351
ryden <howl+git@werewolf.computer>
2020-04-16 05:50:44 +0200

    add more sheri.bot routes

--- src/sheri.js
+++ src/sheri.js
@@ -13,12 +13,10 @@ class Sheri extends Base {
     console.log(this.apikey);
     let headersKey = {
       "User-Agent": this.ua,
-      // "Content-Type": "application/json",
       Authorization: this.apikey,
     };
     let headers_keyless = {
       "User-Agent": this.ua,
-      //"Content-Type": "application/json",
     };
     let headers;
     if (["yiff", "murr"].includes(endpoint)) {
@@ -35,8 +33,207 @@ class Sheri extends Base {
     return res.body;
   }
   // ! Animals
+  get bunny() {
+    return this.request("bunny");
+  }
+  get cat() {
+    return this.request("cat");
+  }
+  get fox() {
+    return this.request("fox");
+  }
+  get husky() {
+    return this.request("husky");
+  }
+  get mur() {
+    return this.request("mur");
+  }
+  get nature() {
+    return this.request("nature");
+  }
+  get pig() {
+    return this.request("pig");
+  }
+  get pig() {
+    return this.request("pig");
+  }
+  get snep() {
+    return this.request("snep");
+  }
+  get turkey() {
+    return this.request("turkey");
+  }
+  get wolf() {
+    return this.request("wolves");
+  }
+  get yiff() {
+    return this.request("yiff");
+  }
+
+  // ! SFW
+
+  // ! NSFW
   get sixtynine() {
     return this.request("69");
+  }
+  get anal() {
+    return this.request("anal");
+  }
+  get bang() {
+    return this.request("bang");
+  }
+  get bisexual() {
+    return this.request("bisexual");
+  }
+  get boob() {
+    return this.request("boob");
+  }
+  get booty() {
+    return this.request("booty");
+  }
+  get christmas() {
+    return this.request("christmas");
+  }
+  get cumflation() {
+    return this.request("cumflation");
+  }
+  get cuntboy() {
+    return this.request("cuntboy");
+  }
+  get dick() {
+    return this.request("dick");
+  }
+  get dp() {
+    return this.request("dp");
+  }
+  get fbound() {
+    return this.request("fbound");
+  }
+  get fcreampie() {
+    return this.request("fcreampie");
+  }
+  get finger() {
+    return this.request("finger");
+  }
+  get fpresentation() {
+    return this.request("fpresentation");
+  }
+  get fseduce() {
+    return this.request("fseduce");
+  }
+  get fsolo() {
+    return this.request("fsolo");
+  }
+  get ftease() {
+    return this.request("ftease");
+  }
+  get gay() {
+    return this.request("gay");
+  }
+  get gif() {
+    return this.request("gif");
+  }
+  get lesbian() {
+    return this.request("lesbian");
+  }
+  get mbound() {
+    return this.request("mbound");
+  }
+  get mcreampie() {
+    return this.request("mcreampie");
+  }
+  get mpresentation() {
+    return this.request("mpresentation");
+  }
+  get mseduce() {
+    return this.request("mseduce");
+  }
+  get msolo() {
+    return this.request("msolo");
+  }
+  get mtease() {
+    return this.request("mtease");
+  }
+  get nboop() {
+    return this.request("nboop");
+  }
+  get nbound() {
+    return this.request("nbound");
+  }
+  get nbrony() {
+    return this.request("nbrony");
+  }
+  get nbulge() {
+    return this.request("nbulge");
+  }
+  get ncomics() {
+    return this.request("ncomics");
+  }
+  get ncuddle() {
+    return this.request("ncuddle");
+  }
+  get nfemboy() {
+    return this.request("nfemboy");
+  }
+  get nfuta() {
+    return this.request("nfuta");
+  }
+  get ngroup() {
+    return this.request("ngroup");
+  }
+  get nhold() {
+    return this.request("nhold");
+  }
+  get nhug() {
+    return this.request("nhug");
+  }
+  get nkiss() {
+    return this.request("nkiss");
+  }
+  get nlick() {
+    return this.request("nlick");
+  }
+  get npats() {
+    return this.request("npats");
+  }
+  get npokemon() {
+    return this.request("npokemon");
+  }
+  get nseduce() {
+    return this.request("nseduce");
+  }
+  get nsolo() {
+    return this.request("nsolo");
+  }
+  get nspank() {
+    return this.request("nspank");
+  }
+  get ntease() {
+    return this.request("ntease");
+  }
+  get ntrap() {
+    return this.request("ntrap");
+  }
+  get petplay() {
+    return this.request("petplay");
+  }
+  get pregnant() {
+    return this.request("pregnant");
+  }
+  get pussy() {
+    return this.request("pussy");
+  }
+  get ride() {
+    return this.request("ride");
+  }
+  get suck() {
+    return this.request("suck");
+  }
+  get tentacles() {
+    return this.request("tentacles");
+  }
+  get toys() {
+    return this.request("toys");
   }
 }
 
